599b8129544708a7e1d5316451eb97c4fe01592f
Anh Hoang Nguyen <iam@hoanganh.tech>
2022-04-19 14:28:59 -0400

    name change

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-html_title = "Anh's Unix Docs"
+html_title = "Unix Convenience Store"
 
 # -- Project information -----------------------------------------------------
 
