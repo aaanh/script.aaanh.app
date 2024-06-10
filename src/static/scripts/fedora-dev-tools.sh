dc5897a3f66cb13f28524395210730b5f510b728
Anh H Nguyen <AnhNguyen@aaanh.com>
2024-06-10 12:46:30 -0400

    Revert some script changes

--- src/static/scripts/fedora-dev-tools.sh
+++ src/static/scripts/fedora-dev-tools.sh
@@ -14,16 +14,16 @@ read -p "Press ENTER to continue..."
 
 echo -e "Installing: Development tools via dnf group"
 read -t 3
-sudo dnf group install -y "Development Tools" && \
+sudo dnf group install -y "Development Tools"
 
 echo -e "Installing: Rust via rustup"
 read -t 3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source "$HOME/.cargo/env" && \
+source "$HOME/.cargo/env"
 
 echo -e "Installing: Golang"
 read -t 3
-sudo dnf install -y go && \
+sudo dnf install -y go
 
 echo -e "Installing: nvm, nodejs, npm, yarn"
 read -t 3
@@ -31,7 +31,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 source ~/.zshrc
 nvm install 20
 nvm use 20
-npm i -g yarn && \
+npm i -g yarn
 
 echo -e "Installing: Visual Studio Code by Microsoft (tm)"
 echo -e "Script provided by Microsoft. See https://code.visualstudio.com/docs/setup/linux"
@@ -39,7 +39,7 @@ read -t 3
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
-sudo dnf install -y code && \
+sudo dnf install -y code
 
 echo -e "Setup script completed successfully."
 exit 0
